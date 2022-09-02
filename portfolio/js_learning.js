cccc1ae2ef8f716a909944f0b65ca3fa73887bd5
bwboyko <98922500+bwboyko@users.noreply.github.com>
2022-09-02 08:59:59 -0700

    Initial upload of files built in Atom IDE

---
+++ portfolio/js_learning.js
@@ -0,0 +1,98 @@
+
+
+//-------------LEAP YEAR-------------------------------------------
+
+// year = prompt("What year is it? We'll check it for a leap year...");
+// if (year % 4 === 0) {
+//   if (year % 100 === 0) {
+//     if (year % 400 === 0) {
+//       console.log("The year " + year + " IS a leap year.");
+//     } else {
+//       console.log("The year " + year + " is NOT a leap year.");
+//     }
+//   } else {
+//     console.log("The year " + year + " IS a leap year.");
+//   }
+// } else {
+//     console.log("The year " + year + " is NOT a leap year.");
+//   }
+
+
+
+//--------------GUEST LIST------------------------
+
+// guestList = ["brendan", "margee", "ruka", "lola"]
+// name = prompt("What is your name: ");
+// name = name.toLowerCase();
+//
+// if (guestList.includes(name)) {
+//   console.log("Welcome to zeh partayyyyyyy.")
+// }
+// else {
+//   console.log("You aren't cool enough.")
+// }
+
+// guestList = ["brendan", "margee", "ruka", "lola"];
+// listLength = guestList.length;
+// console.log(listLength);
+// randomNumber = Math.floor((Math.random() * listLength));
+// console.log(randomNumber);
+//
+// console.log("It looks like "  + guestList[randomNumber] + " is paying for lunch!");
+
+//----------------FIZZ BUZZ-------------------------------
+
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0) {
+//     if (i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     }
+//     else {
+//       console.log("Fizz");
+//     }
+//   }
+//   else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   }
+//   else {
+//     console.log(i);
+//   }
+// }
+//
+//
+// var output = [];
+//
+// function fizzBuzz() {
+//   var nextNum = output.length + 1;
+//   if (nextNum % 3 === 0) {
+//     if (nextNum % 5 === 0) {
+//       output.push("FizzBuzz");
+//     }
+//     else {
+//       output.push("Fizz");
+//     }
+//   }
+//   else if (nextNum % 5 === 0) {
+//     output.push("Buzz");
+//   }
+//   else {
+//     output.push(nextNum);
+//   }
+//   console.log(output);
+// }
+
+
+
+// // ---------------- Beer ------------//
+// function beer() {
+//   i = 100
+//
+//   while (i > 0) {
+//     console.log(i + " bottles of beer on the wall.");
+//     console.log(i + " bottles of beer.");
+//     i--;
+//     console.log("Take one down and pass it around... " + i + " bottles of beer on the wall!");
+//   }
+//   console.log("Awwww we're out of beer...")
+// }
